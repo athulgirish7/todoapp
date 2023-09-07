37f65fcb8d69875da1fd065fb1ff504de89678c8
Anusha N T <anushant5@gmail.com>
2023-09-07 16:53:33 +0530

    Joy validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Joi from 'joi';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'todoapp';
+
+  schema = Joi.object().keys({
+    username: Joi.string().alphanum().min(3).max(6).required().messages({
+      'string.base': 'Base error',
+      'string.min': 'min error',
+      'string.max': 'max error',
+      'any.required': 'required error',
+      'string.alphanum': 'alpha numeric error',
+    }),
+  });
+
+  ngOnInit(): void {
+    const result = this.schema.validate({ username: ' ' });
+    console.log(result.error);
+  }
 }
